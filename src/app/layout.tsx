4db2f6756e7f49c94fbe69cd17ac4cc7d5d76723
guesung <gueit214@naver.com>
2025-07-13 23:02:42 +0900

    feat: stagewise 설치
    ref : https://github.com/stagewise-io/stagewise

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { StagewiseToolbar } from "@stagewise/toolbar-next";
+import ReactPlugin from "@stagewise-plugins/react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -27,6 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <StagewiseToolbar
+          config={{
+            plugins: [ReactPlugin],
+          }}
+        />
         {children}
       </body>
     </html>
